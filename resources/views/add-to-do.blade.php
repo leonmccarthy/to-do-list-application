76956f5439e2d982d314deba5aa83f3dcdc91ca9
leonmccarthy <leonmccarthy05@gmail.com>
2025-02-18 20:58:21 +0300

    Added viewing add to do page

--- resources/views/add-to-do.blade.php
+++ resources/views/add-to-do.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Add To Do') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in. Welcome!') }}
+                    {{ __('This is to do page') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,8 +9,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// VIEW ADD TO DO PAGE
+Route::get('/add-to-do', function (){
+    return view('add-to-do');
+})->name('add-to-do');
